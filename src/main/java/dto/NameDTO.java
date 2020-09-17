455818d332e55240796a23b6c6fda1a73c569714
jacmac2812 <simonsen.jac@icloud.com>
2020-09-17 16:19:47 +0200

    changes to NameDTO, Name and NameResource

--- src/main/java/dto/NameDTO.java
+++ src/main/java/dto/NameDTO.java
@@ -5,10 +5,28 @@
  */
 package dto;
 
+import entities.Name;
+
 /**
  *
  * @author jacobsimonsen
  */
 public class NameDTO {
+    
+    private int id;
+    private String name;
+
+    public NameDTO(Name name) {
+        this.id = name.getId();
+        this.name = name.getName();
+    }    
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
     
 }
--- src/main/java/rest/NameResource.java
+++ src/main/java/rest/NameResource.java
@@ -5,10 +5,98 @@
  */
 package rest;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dto.JokeDTO;
+import dto.NameDTO;
+import entities.Name;
+import facades.NameFacade;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import utils.EMF_Creator;
+
 /**
  *
  * @author jacobsimonsen
  */
+@Path("names")
 public class NameResource {
+
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+
+    //får fejl på denne 
+//    //An alternative way to get the EntityManagerFactory, whithout having to type the details all over the code
+//    //EMF = EMF_Creator.createEntityManagerFactory(DbSelector.DEV, Strategy.CREATE);
+//    private static final NameFacade FACADE = NameFacade.getNameFacade(EMF);
+//    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String demo() {
+        return "{\"msg\":\"Hello World\"}";
+    }
+
+    //får fejl på denne
+//    @Path("count")
+//    @GET
+//    @Produces({MediaType.APPLICATION_JSON})
+//    public String getNameCount() {
+//        long count = FACADE.getNameCount();
+//        //System.out.println("--------------->"+count);
+//        return "{\"count\":" + count + "}";  //Done manually so no need for a DTO
+//    }
+
+    //får fejl på denne 
+//    @Path("all")
+//    @GET
+//    @Produces({MediaType.APPLICATION_JSON})
+//    public String getAllNames() {
+//        List<Name> names = FACADE.getAllNames();
+//        List<NameDTO> nameDTOs = new ArrayList();
+//        for (Name Name : names) {
+//            NameDTO NameDTO = new NameDTO(name);
+//            NameDTOs.add(nameDTO);
+//        }
+//        return GSON.toJson(nameDTOs);
+//    }
+
     
+    //skal ummelbart ikke bruges
+//    @Path("type/{type}")
+//    @GET
+//    @Produces({MediaType.APPLICATION_JSON})
+//    public String getJokesByType(@PathParam("type") String type) {
+//        try {
+//            List<Joke> jokes = FACADE.getJokesByType(type);
+//            List<JokeDTO> jokeDTOs = new ArrayList();
+//            for (Joke joke : jokes) {
+//                JokeDTO jokeDTO = new JokeDTO(joke);
+//                jokeDTOs.add(jokeDTO);
+//            }
+//            return GSON.toJson(jokeDTOs);
+//        } catch (NoResultException | NullPointerException e) {
+//            return GSON.toJson(null);
+//        }
+//    }
+
+    
+    //får fejl på denne
+//    @Path("{id}")
+//    @GET
+//    @Produces({MediaType.APPLICATION_JSON})
+//    public String getNameById(@PathParam("id") int id) {
+//        try {
+//            Name name = FACADE.getNameById(id);
+//            return GSON.toJson(name);
+//        } catch (NoResultException | NullPointerException e) {
+//            return GSON.toJson(null);
+//        }
+//    }
 }
